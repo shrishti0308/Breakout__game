2f39426428232ccdc3cc3416e65fb888069d756f
shrishti0308 <shrishtimahor2805@gmail.com>
2024-07-05 20:55:42 +0530

    move the ball

--- main.js
+++ main.js
@@ -1,17 +1,49 @@
 const canvas = document.getElementById("myCanvas");
             const ctx = canvas.getContext("2d");
-            ctx.beginPath();
-            ctx.rect(40,80,70,70);
-            ctx.fillStyle = "#FF0000";
-            ctx.fill();
-            ctx.closePath();
-            ctx.beginPath();
-            ctx.arc(240,200,35,0,Math.PI * 2, false);
-            ctx.fillStyle = "green";
-            ctx.fill();
-            ctx.closePath();
-            ctx.beginPath();
-            ctx.rect(180, 10, 130, 60);
-            ctx.strokeStyle = "rgb(0 0 255 / 50%)";
-            ctx.stroke();
-            ctx.closePath();
+            // ctx.beginPath();
+            // ctx.rect(40,80,70,70);
+            // ctx.fillStyle = "#FF0000";
+            // ctx.fill();
+            // ctx.closePath();
+            // ctx.beginPath();
+            // ctx.arc(240,200,35,0,Math.PI * 2, false);
+            // ctx.fillStyle = "green";
+            // ctx.fill();
+            // ctx.closePath();
+            // ctx.beginPath();
+            // ctx.rect(180, 10, 130, 60);
+            // ctx.strokeStyle = "rgb(0 0 255 / 50%)";
+            // ctx.stroke();
+            // ctx.closePath();
+            let x = canvas.width / 2;
+            let y = canvas.height - 30;
+            const dx = 2;
+            const dy = -2;
+
+            function drawBall(){
+                ctx.beginPath();
+                ctx.arc(x,y,10,0,Math.PI * 2);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+
+            }
+
+            function draw(){
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                drawBall();
+                x += dx;
+                y += dy;
+                
+            }
+
+            function startGame(){
+                setInterval(draw, 10);
+
+            }
+
+            document.getElementById("runButton").addEventListener("click",function(){
+                startGame();
+                this.disabled = true;
+            });
+            
