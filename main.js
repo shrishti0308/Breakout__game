ca387a313d2c5bef2d18f194b761a413911eba2f
shrishti0308 <shrishtimahor2805@gmail.com>
2024-07-05 20:05:07 +0530

    create the canvas and draw on it

---
+++ main.js
@@ -0,0 +1,17 @@
+const canvas = document.getElementById("myCanvas");
+            const ctx = canvas.getContext("2d");
+            ctx.beginPath();
+            ctx.rect(40,80,70,70);
+            ctx.fillStyle = "#FF0000";
+            ctx.fill();
+            ctx.closePath();
+            ctx.beginPath();
+            ctx.arc(240,200,35,0,Math.PI * 2, false);
+            ctx.fillStyle = "green";
+            ctx.fill();
+            ctx.closePath();
+            ctx.beginPath();
+            ctx.rect(180, 10, 130, 60);
+            ctx.strokeStyle = "rgb(0 0 255 / 50%)";
+            ctx.stroke();
+            ctx.closePath();
