0282606a01a03098fd17842e63a4382035c7e921
shrishti0308 <shrishtimahor2805@gmail.com>
2024-07-05 21:13:29 +0530

    bounce off the walls

--- main.js
+++ main.js
@@ -19,10 +19,11 @@ const canvas = document.getElementById("myCanvas");
             let y = canvas.height - 30;
             const dx = 2;
             const dy = -2;
+            const ballRadius = 10;
 
             function drawBall(){
                 ctx.beginPath();
-                ctx.arc(x,y,10,0,Math.PI * 2);
+                ctx.arc(x,y,ballRadius,0,Math.PI * 2);
                 ctx.fillStyle = "#0095DD";
                 ctx.fill();
                 ctx.closePath();
@@ -31,10 +32,15 @@ const canvas = document.getElementById("myCanvas");
 
             function draw(){
                 ctx.clearRect(0, 0, canvas.width, canvas.height);
-                drawBall();
+                drawBall();    
+            if(x + dx > canvas.width - ballRadius || x + dx < ballRadius){
+                dx = -dx;
+            }
+            if(y + dy > canvas.height - ballRadius || y + dy < ballRadius){
+                dy = -dy;
+            }   
                 x += dx;
                 y += dy;
-                
             }
 
             function startGame(){
