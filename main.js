848bb55bb70dafe782e26c85df9242fcaddee2ee
shrishti0308 <shrishtimahor2805@gmail.com>
2024-07-05 21:52:56 +0530

    paddle and keyboard controls

--- main.js
+++ main.js
@@ -20,7 +20,31 @@ const canvas = document.getElementById("myCanvas");
             const dx = 2;
             const dy = -2;
             const ballRadius = 10;
+            const paddleHeight = 10;
+            const paddleWidth = 75;
+            let paddleX = (canvas.width - paddleWidth) / 2;
+            let rightPressed = false;
+            let leftPressed = false;
 
+            document.addEventListener("keydown" , keyDownHandler, false);
+            document.addEventListener("keyup" , keyUpHandler, false);
+
+            function keyDownHandler(e){
+                if(e.key == "Right" || e.key == "ArrowRight"){
+                    rightPressed = true;
+                }else if(e.key == "Left" || e.key == "ArrowLeft"){
+                    leftPressed = true;
+                }
+            }
+
+            function keyUpHandler(e){
+                if(e.key == "Right" || e.key == "ArrowRight"){
+                    rightPressed = false;
+                }else if(e.key == "Left" || e.key == "ArrowLeft"){
+                    leftPressed = false;
+                }
+            }
+            
             function drawBall(){
                 ctx.beginPath();
                 ctx.arc(x,y,ballRadius,0,Math.PI * 2);
@@ -30,26 +54,59 @@ const canvas = document.getElementById("myCanvas");
 
             }
 
+            function drawPaddle(){
+                ctx.beginPath();
+                ctx.clearRect(paddleX, canvas.height - paddleHeight,  paddleWidth, paddleHeight);
+                ctx.fillStyle = "0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
+
+          
+
             function draw(){
                 ctx.clearRect(0, 0, canvas.width, canvas.height);
                 drawBall();    
+                drawPaddle();
             if(x + dx > canvas.width - ballRadius || x + dx < ballRadius){
                 dx = -dx;
             }
             if(y + dy > canvas.height - ballRadius || y + dy < ballRadius){
                 dy = -dy;
             }   
+            // if (rightPressed) {
+            //     paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth);
+            //   } else if (leftPressed) {
+            //     paddleX = Math.max(paddleX - 7, 0);
+            //   }
+
+              if (rightPressed) {
+                paddleX += 7;
+                if (paddleX + paddleWidth > canvas.width) {
+                  paddleX = canvas.width - paddleWidth;
+                }
+              } else if (leftPressed) {
+                paddleX -= 7;
+                if (paddleX < 0) {
+                  paddleX = 0;
+                }
+              }
+            
+              
                 x += dx;
                 y += dy;
             }
+
 
             function startGame(){
                 setInterval(draw, 10);
 
             }
+            
 
             document.getElementById("runButton").addEventListener("click",function(){
                 startGame();
                 this.disabled = true;
             });
+
             
