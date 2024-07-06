acc0ddcdeacc36b6e64c90ec75bfd7a8e3d57961
shrishti0308 <shrishtimahor2805@gmail.com>
2024-07-06 20:00:27 +0530

    changes

--- main.js
+++ main.js
@@ -137,13 +137,13 @@ const canvas = document.getElementById("myCanvas");
             function drawScore(){
                 ctx.font = "16px Arial";
                 ctx.fillStyle = "0095DD";
-                ctx.fillText(`Score: $(score)` , 8, 20);
+                ctx.fillText("Score: " + score , 8, 20);
             }
             
             function drawLives(){
                 ctx.font = "16px Arial";
                 ctx.fillStyle = "0095DD";
-                ctx.fillText(`Lives: $(lives)` , canvas.width - 65, 20);
+                ctx.fillText("Lives: " + lives , canvas.width - 65, 20);
             }
             
 
